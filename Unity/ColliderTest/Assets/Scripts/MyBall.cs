0550f65878ddb5b0456ffe93d2456f368c35be65
Seung@gram-win <ncraig@naver.com>
2019-04-23 12:31:06 +0900

    initial commit

---
+++ Unity/ColliderTest/Assets/Scripts/MyBall.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.EventSystems;
+
+
+public class MyBall : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody rb;
+	
+
+	private int count1;
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        speed = 10;
+		
+        //joystick = FindObjectOfType<Joystick>();
+    }
+
+
+    // Update is called once per frame
+
+    void Update()
+    {
+
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement1 = new Vector3(moveHorizontal, 0, moveVertical);
+		rb.AddForce(movement1 * speed);
+		
+    }
+
+}
---
+++ Unity/ColliderTest/Assets/Scripts/ToggleLight.cs
@@ -0,0 +1,24 @@
+﻿// ToggleLight.cs
+// Turns the light component of this object on/off when the user presses and releases the `L` key.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleLight : MonoBehaviour {
+
+	Light light1;
+
+	// Use this for initialization
+	void Start () {
+		light1 = GetComponent<Light> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// Toggle light on/off when L key is pressed.
+		if (Input.GetKeyUp (KeyCode.L)) {
+			GetComponent<Light>().enabled = !GetComponent<Light>().enabled;
+		}
+	}
+}
