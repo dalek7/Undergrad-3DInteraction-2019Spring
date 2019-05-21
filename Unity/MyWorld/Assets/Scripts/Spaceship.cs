eb85867c9f346aa702b4108562cdadebb1a64e1f
Seung@gram-win <ncraig@naver.com>
2019-05-21 22:08:08 +0900

    make object move back and forth

---
+++ Unity/MyWorld/Assets/Scripts/Spaceship.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spaceship : MonoBehaviour
+{
+	private Vector3 pos0;
+	int cnt;
+    // Start is called before the first frame update
+    void Start()
+    {
+		cnt = 0;
+        pos0 = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		//transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+		//transform.position.y = cnt * 0.01;
+		//transform.position = new Vector3(pos0.x, pos0.y+ (float)0.01*cnt ,pos0.z);
+		transform.position = new Vector3(pos0.x, 
+							pos0.y+ (float) 0.5f* Mathf.Sin(0.01f*cnt) ,
+							//pos0.y+ (float) 0.5f* Mathf.Sin(Time.deltaTime) ,
+							pos0.z);
+		
+		cnt = cnt+1;
+    }
+}
