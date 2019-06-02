73c333736cfa4ec520984819341a5dfd085119b4
Seung@gram-win <ncraig@naver.com>
2019-06-03 00:28:53 +0900

    added Bullet.cs

---
+++ Unity/MyWorld/Assets/Scripts/Bullet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float bulletSpeed = 20.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(this.gameObject, 2.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position += this.transform.forward * bulletSpeed * Time.deltaTime;
+    }
+}
